8517f2bc230e1f677663153c3614ad6e8c05a824
Paul <paulswil@cisco.com>
2018-01-30 16:18:11 -0500

    create functions to start and cleanup capture, move them to new module file

---
+++ capture.py
@@ -0,0 +1,40 @@
+# functions to set up, start, and stop packet capture in XE device
+# note - may need to change syntax of export command for CSR
+
+import cli
+
+
+def acl_command(proto,src,dst):
+    if (src == 'any') and (dst == 'any'):
+        command = '''ip access-list extended PKT_CAP
+                           permit %s %s %s''' %(proto,src,dst)
+    elif (src == 'any') and (dst != 'any'):
+        command = '''ip access-list extended PKT_CAP
+                           permit %s %s host %s''' %(proto,src,dst)
+    elif (src != 'any') and (dst != 'any'):
+        command = '''ip access-list extended PKT_CAP
+                           permit %s host %s %s''' %(proto,src,dst)
+    else:
+        command = '''ip access-list extended PKT_CAP
+                           permit %s host %s host %s''' %(proto,src,dst)
+
+    return command
+
+
+def start_capture(proto,src,dst):
+    configuration = acl_command(proto, src, dst)
+    cli.configure(configuration)
+    cli.execute(
+        "monitor capture PKT_CAP access-list PKT_CAP buffer circular size 100")
+    cmd = "monitor capture PKT_CAP interface %s both" % args.interface
+    cli.execute(cmd)
+    cli.execute("monitor capture PKT_CAP clear")
+    cli.execute("monitor capture PKT_CAP start")
+
+
+def cap_cleanup():
+    cli.execute("monitor capture PKT_CAP stop")
+    cmd = "monitor capture PKT_CAP export location flash:%s" % filename  # changed syntax to fit Cat9K
+    cli.execute(cmd)
+    configuration = 'no ip access-list extended PKT_CAP'  # delete capture ACL so next capture has a fresh filter
+    cli.configure(configuration)
---
+++ xepacketcap.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import cli
+import sys
+# from csr_aws_guestshell import cag
+import argparse
+import time
+from capture import *
+
+parser = argparse.ArgumentParser(
+    description="Capture Packets and upload file to S3")
+parser.add_argument('interface', help='The interface to capture from')
+# parser.add_argument('bucket', help='The name of the bucket to upload to')
+parser.add_argument('protocol', help='IP TCP UDP OSPF EIGRP ICMP')
+parser.add_argument('src', help='x.x.x.x or any')
+parser.add_argument('dst', help='x.x.x.x or any')
+parser.add_argument('filename', help='Filename to upload to bucket')
+parser.add_argument('--seconds', help='Seconds to capture', default=10)
+args = parser.parse_args()
+
+#bucket = args.bucket
+filename = args.filename
+proto = args.protocol
+src = args.src
+dst = args.dst
+
+start_capture(proto,src,dst)
+
+for i in range(0, int(args.seconds)):
+    time.sleep(1)
+    sys.stdout.write("\r%d secs" % (i + 1))
+    sys.stdout.flush()
+
+print "\n"
+
+cap_cleanup()
+
+
+# cag().upload_file(bucket, filename)
