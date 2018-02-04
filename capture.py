4877d038a81b6ab07890f544ed653cf5c5b77fe7
Paul <paulswil@cisco.com>
2018-02-04 09:55:11 -0500

    added cap_wait() function, modified cap_cleanup()

--- capture.py
+++ capture.py
@@ -4,6 +4,8 @@
 import cli
 import subprocess
 import sys
+import sqlite3
+import time
 
 
 def acl_command(proto,src,dst):
@@ -34,12 +36,18 @@ def start_capture(proto,src,dst):
     cli.execute("monitor capture PKT_CAP start")
 
 
-def cap_cleanup():
+def cap_cleanup(jobID):
     cli.execute("monitor capture PKT_CAP stop")
     cmd = "monitor capture PKT_CAP export location flash:%s" % filename  # changed syntax to fit Cat9K
     cli.execute(cmd)
     configuration = 'no ip access-list extended PKT_CAP'  # delete capture ACL so next capture has a fresh filter
     cli.configure(configuration)
+    conn = sqlite3.connect('capdb.db')
+    c = conn.cursor()
+    c.execute('DELETE from CAPJOBS where JOB_ID = %d', (jobID))
+    conn.commit()
+    conn.close()
+
 
 def cap_check():
     output = subprocess.check_output('ps -ef | grep xepacketcap.py', shell=True)
@@ -49,5 +57,36 @@ def cap_check():
         sys.exit()
     else:
         pass
+
+def cap_wait(jobID,duration):
+    conn = sqlite3.connect('capdb.db')
+    c = conn.cursor()
+    count = c.execute('select count(*) from jobs;')
+    rows = c.fetchone()
+    numrows = rows[0]
+    if numrows == 0:
+        break # if there are no jobs in the queue, GO!!!!
+    else:
+        #calculate wait time based on duration of jobs already in queue
+        c.execute('select duration from jobs')
+        output = c.fetchall()
+        wait = 10 # add 10 second buffer to wait time
+        for i in output:
+            wait = wait + output[i][0]
+        #add my job to capdb
+        conn.execute('INSERT INTO CAPJOBS (JOBID, DURATION) VALUES' (%d, %d)', (jobID, duration))
+        conn.commit()
+        conn.close()
+        #begin wait counter
+        for i in range(0, wait):
+            time.sleep(1)
+            '''sys.stdout.write("\r%d secs" % (i + 1))
+               sys.stdout.flush()'''
+        # when wait time completes, proceed with capture
+        # future enhancement: check db again to verify I'm at the top of the list, if yes then go ahead
+
+
+
+
 
 
