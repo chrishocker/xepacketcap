923e46a965230f80c412f92e829eaa88ec18e602
luisra33 <luisgarcia33@gmail.com>
2018-02-21 17:36:11 -0500

    Update pcap_api.py

--- pcap_api.py
+++ pcap_api.py
@@ -1,197 +1,44 @@
-from flask import Flask, url_for, jsonify, request,\
-    make_response, copy_current_request_context
+from flask import Flask, url_for, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from capture import *
-import uuid
-import functools
 from threading import Thread
 from random import randint
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///network.db'
-db = SQLAlchemy(app)
+
+@app.route('/form', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        job_id = randint(100000,999999)
+        iface = request.form['iface']
+        proto = request.form['proto']
+        src = request.form['src']
+        dst = request.form['dst']
+        duration = request.form['duration']
+        bucket = request.form['bucket']
+        filename = request.form['filename']
+        add_capture(job_id,iface,proto,src,dst,duration,bucket,filename)
 
 
-background_tasks = {}
-app.config['AUTO_DELETE_BG_TASKS'] = True
+        return '''<h1>Inteface: {}</h1>
+                  <h1>Protocol: {}</h1>
+                  <h1>Source: {}</h1>
+                  <h1>Destination: {}</h1>
+                  <h1>Duration in Seconds: {}</h1>
+                  <h1>AWS S3 Bucket: {}</h1>
+                  <h1>Filename: {}</h1>'''.format(iface, proto, src, dst, duration, bucket, filename)
 
-
-class ValidationError(ValueError):
-    pass
-
-
-class Pcap(db.Model):
-    __tablename__ = 'pcap_jobs'
-    id = db.Column(db.Integer, primary_key=True)
-    interface = db.Column(db.String(64))
-    bucket = db.Column(db.String(64))
-    protocol = db.Column(db.String(64))
-    src = db.Column(db.String(64))
-    dst = db.Column(db.String(64))
-    filename = db.Column(db.String(64), unique=True)
-    seconds = db.Column(db.String(64))
-
-    def get_url(self):
-        return url_for('get_pcap', id=self.id, _external=True)
-
-    def export_data(self):
-        return {
-            'self_url': self.get_url(),
-            'interface': self.interface,
-            'bucket': self.bucket,
-            'protocol': self.protocol,
-            'src': self.src,
-            'dst': self.dst,
-            'filename': self.filename,
-            'seconds': self.seconds
-            }
-
-    def import_data(self, data):
-        try:
-            self.interface = data['interface']
-            self.bucket = data['bucket']
-            self.protocol = data['protocol']
-            self.src = data['src']
-            self.dst = data['dst']
-            self.filename = data['filename']
-            self.seconds = data['seconds']
-        except KeyError as e:
-            raise ValidationError('Invalid job: missing ' + e.args[0])
-        return self
-
-
-def background(f):
-    """Decorator that runs the wrapped function as a background task. It is
-    assumed that this function creates a new resource, and takes a long time
-    to do so. The response has status code 202 Accepted and includes a Location
-    header with the URL of a task resource. Sending a GET request to the task
-    will continue to return 202 for as long as the task is running. When the task
-    has finished, a status code 303 See Other will be returned, along with a
-    Location header that points to the newly created resource. The client then
-    needs to send a DELETE request to the task resource to remove it from the
-    system."""
-    @functools.wraps(f)
-    def wrapped(*args, **kwargs):
-        # The background task needs to be decorated with Flask's
-        # copy_current_request_context to have access to context globals.
-        @copy_current_request_context
-        def task():
-            global background_tasks
-            try:
-                # invoke the wrapped function and record the returned
-                # response in the background_tasks dictionary
-                background_tasks[id] = make_response(f(*args, **kwargs))
-            except:
-                # the wrapped function raised an exception, return a 500
-                # response
-                background_tasks[id] = make_response(internal_server_error())
-
-        # store the background task under a randomly generated identifier
-        # and start it
-        global background_tasks
-        id = uuid.uuid4().hex
-        background_tasks[id] = Thread(target=task)
-        background_tasks[id].start()
-
-        # return a 202 Accepted response with the location of the task status
-        # resource
-        return jsonify({}), 202, {'Location': url_for('get_task_status', id=id)}
-    return wrapped
-
-
-@app.route('/pcap_jobs/', methods=['GET'])
-def get_pcaps():
-    return jsonify({'pcap': [pcap.get_url()
-                                  for pcap in Pcap.query.all()]})
-
-@app.route('/pcap_jobs/<int:id>', methods=['GET'])
-def get_pcap(id):
-    return jsonify(Pcap.query.get_or_404(id).export_data())
-
-
-@app.route('/pcap_jobs/', methods=['POST'])
-def new_pcap():
-    pcap = Pcap()
-    pcap.import_data(request.json)
-    db.session.add(pcap)
-    db.session.commit()
-    return jsonify({}), 201, {'Location': pcap.get_url()}
-
-@app.route('/pcap_jobs/<int:id>', methods=['PUT'])
-def edit_pcap(id):
-    pcap = Pcap.query.get_or_404(id)
-    pcap.import_data(request.json)
-    db.session.add(pcap)
-    db.session.commit()
-    return jsonify({})
-
-@app.route('/pcap_jobs/<int:id>/run', methods=['GET'])
-#@background
-def run_pcap_job(id):
-    pcap = Pcap.query.get_or_404(id)
-    jobID = randint(100000,999999)
-    interface = pcap.interface
-    bucket = pcap.bucket
-    protocol = pcap.protocol
-    src = pcap.src
-    dst = pcap.dst
-    filename = pcap.filename
-    seconds = pcap.seconds
-    wait = cap_wait(jobID, seconds)
-    start_capture(interface,protocol,src,dst,jobID,wait,seconds)
-    cap_cleanup(jobID,bucket,filename)
-    job = str(jobID)
-    #return jsonify(Pcap.query.get_or_404(id).export_data()), 201, {'Location': pcap.get_url()}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Packet Capture Application</title>
-    </head>
-    <style>
-    body {background-color: Gray;}
-    h1   {
-          color: Black;
-          background-color:LightGray;
-         }
-    p    {
-          color: Black;
-          background-color:LightGray;
-         }
-    </style>
-    <body>
-        <h1>Packet capure with Job ID #''' + job + ''' has been completed<h1>
-        <p>PCAP file ''' + filename + ''' has been uploaded to AWS S3 bucket ''' + bucket + '''. </p>
-        <p><a href="https://s3-us-west-1.amazonaws.com/''' + bucket + '''/''' + filename + '''">Download PCAP File</a></p>
-    </body>
-</html>'''
-
-
-@app.route('/status/<id>', methods=['GET'])
-def get_task_status(id):
-    """Query the status of an asynchronous task."""
-    # obtain the task and validate it
-    global background_tasks
-    rv = background_tasks.get(id)
-    if rv is None:
-        return not_found(None)
-
-    # if the task object is a Thread object that means that the task is still
-    # running. In this case return the 202 status message again.
-    if isinstance(rv, Thread):
-        return jsonify({}), 202, {'Location': url_for('get_task_status', id=id)}
-
-        # If the task object is not a Thread then it is assumed to be the response
-        # of the finished task, so that is the response that is returned.
-        # If the application is configured to auto-delete task status resources once
-        # the task is done then the deletion happens now, if not the client is
-        # expected to send a delete request.
-    if app.config['AUTO_DELETE_BG_TASKS']:
-        del background_tasks[id]
-    return rv
-
-
+    return '''<form method="POST">
+                  Interface: <input type="text" name="iface"><br>
+                  Protocol: <input type="text" name="proto"><br>
+                  Source: <input type="text" name="src"><br>
+                  Destination: <input type="text" name="dst"><br>
+                  Duration: <input type="text" name="duration"><br>
+                  Bucket: <input type="text" name="bucket"><br>
+                  Filename: <input type="text" name="filename"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(host='0.0.0.0', debug=True)
