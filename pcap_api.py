7f7c1f5eff6678f9a2291232e4aa27e6c4aa3d21
luisra33 <luisgarcia33@gmail.com>
2018-02-22 17:58:57 -0500

    Update pcap_api.py

--- pcap_api.py
+++ pcap_api.py
@@ -1,14 +1,15 @@
 from flask import Flask, url_for, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from capture import *
+from get_capture import *
 from threading import Thread
 from random import randint
 
 
 app = Flask(__name__)
 
-@app.route('/form', methods=['GET', 'POST']) #allow both GET and POST requests
-def form_example():
+@app.route('/pcap', methods=['GET', 'POST']) #allow both GET and POST requests
+def run_pcap():
     if request.method == 'POST':  #this block is only entered when the form is submitted
         job_id = randint(100000,999999)
         iface = request.form['iface']
@@ -39,6 +40,40 @@ def form_example():
                   Filename: <input type="text" name="filename"><br>
                   <input type="submit" value="Submit"><br>
               </form>'''
+
+@app.route('/status', methods=['GET', 'POST'])
+def pcap_status():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        job_id = request.form['job_id']
+        get_capture(job_id)
+
+
+        return '''<h1>Enter Job ID: {}</h1>'''.format(job_id)
+
+    return jsonify({result})
+
+@app.route('/pcap_json/', methods=['POST'])
+def run_pcap_json():
+    req_data = request.get_json()
+    job_id = randint(100000,999999)
+    job = str(job_id)
+    iface = req_data['iface']
+    proto = req_data['proto']
+    src = req_data['src']
+    dst = req_data['dst']
+    duration = req_data['duration']
+    bucket = req_data['bucket']
+    filename = req_data['filename']
+    add_capture(job_id,iface,proto,src,dst,duration,bucket,filename)
+    return "This is your job ID: " + job
+
+@app.route('/pcap_json/', methods=['GET'])
+def status_json():
+    req_data = request.get_json()
+    job_id = req_data['job_id']
+    get_capture(job_id)
+    return result
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
