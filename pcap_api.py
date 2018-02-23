71d60b9d6a283f25f1ad1ed535f9d327b2d0c8cc
Jason King <jk1ng@yahoo.com>
2018-02-23 10:42:25 -0800

    Changed pcap API route points to return JSON

--- pcap_api.py
+++ pcap_api.py
@@ -67,14 +67,14 @@ def run_pcap_json():
     bucket = req_data['bucket']
     filename = req_data['filename']
     add_capture(job_id,iface,proto,src,dst,duration,bucket,filename)
-    return "This is your job ID: " + job
+    return jsonify( {'job_id': job} )
 
 @app.route('/status_json', methods=['POST'])
 def status_json():
     req_data = request.get_json()
     job_id = req_data['job_id']
-    job = get_capture(job_id)
-    return job
+    id, job_id, iface, proto, src, dst, duration, bucket, filename, url, status = get_capture(job_id)
+    return jsonify( {'job_id': job_id, 'iface': iface, 'proto': proto, 'src': src, 'dst': dst, 'duration': duration, 'url': url, 'status': status} )
 
 
 if __name__ == '__main__':
